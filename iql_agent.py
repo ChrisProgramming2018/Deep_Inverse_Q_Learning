fix softmax bug (nn.CrossEntropy loss has one soft)
(nn.CrossEntropy loss has one softmax)

iql_agent.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 import random
 from collections import namedtuple, deque
-from models import QNetwork, RNetwork, PolicyNetwork, Classifier
+from models import QNetwork, RNetwork, Classifier
 import torch
 import torch.nn  as nn
 import torch.nn.functional as F
@@ -19,15 +19,16 @@ class Agent():
         self.action_dim = action_dim
         self.seed = 0
         self.device = 'cuda'
+        print("cuda ", torch.cuda.is_available())
         self.batch_size = config["batch_size"]
         self.lr = config["lr"]
         self.gamma = 0.99
-        self.q_shift_local = QNetwork(state_size, action_size, self.seed).to(self.device)
-        self.q_shift_target = QNetwork(state_size, action_size, self.seed).to(self.device)
-        self.Q_local = QNetwork(state_size, action_size, self.seed).to(self.device)
-        self.Q_target = QNetwork(state_size, action_size, self.seed).to(self.device)
-        self.R_local = RNetwork(state_size,action_size, self.seed).to(self.device)
-        self.R_target = RNetwork(state_size, action_size, self.seed).to(self.device)
+        self.q_shift_local = QNetwork(state_size, action_dim, self.seed).to(self.device)
+        self.q_shift_target = QNetwork(state_size, action_dim, self.seed).to(self.device)
+        self.Q_local = QNetwork(state_size, action_dim, self.seed).to(self.device)
+        self.Q_target = QNetwork(state_size, action_dim, self.seed).to(self.device)
+        self.R_local = RNetwork(state_size,action_dim, self.seed).to(self.device)
+        self.R_target = RNetwork(state_size, action_dim, self.seed).to(self.device)
         self.predicter = Classifier(state_size, action_dim, self.seed).to(self.device)
         # optimizer
         self.optimizer_q_shift = optim.Adam(self.q_shift_local.parameters(), lr=self.lr)
@@ -37,6 +38,7 @@ class Agent():
         pathname = "lr {} batch_size {} seed {}".format(self.lr, self.batch_size, self.seed)
         tensorboard_name = str(config["locexp"]) + '/runs/' + pathname 
         self.writer = SummaryWriter(tensorboard_name)
+        print("summery writer ", tensorboard_name)
         self.average_prediction = deque(maxlen=100)
         self.average_same_action = deque(maxlen=100)
         self.steps = 0
@@ -65,7 +67,7 @@ class Agent():
             self.a3 += 1
 
     def learn(self, memory):
-        states, next_states, actions = memory.expert_policy(self.batch_size)
+        states, next_states, actions, dones = memory.expert_policy(self.batch_size)
         self.steps += 1
         #print("  ")
         #print("  ")
@@ -76,7 +78,7 @@ class Agent():
         self.state_action_frq(states, actions)
         self.compute_shift_function(states, next_states, actions)
         self.compute_r_function(states, actions)
-        self.compute_q_function(states, next_states, actions)
+        self.compute_q_function(states, next_states, actions, dones)
         # update local nets 
         self.soft_update(self.Q_local, self.Q_target)
         self.soft_update(self.q_shift_local, self.q_shift_target)
@@ -98,8 +100,9 @@ class Agent():
         """
         same_state_predition = 0
         for i in range(100):
-            states, next_states, actions = memory.expert_policy(1)
+            states, next_states, actions, done = memory.expert_policy(1)
             output = self.predicter(states.unsqueeze(0))
+            output = F.softmax(output, dim=2)
             # create one hot encode y from actions
             y = actions.type(torch.long)[0][0].data
             p =torch.argmax(output.data).data
@@ -133,14 +136,16 @@ class Agent():
         self.optimizer_pre.step()
         self.writer.add_scalar('Predict_loss', loss, self.steps)
 
-    def get_action_prob(self, states, actions, dim=False):
+    def get_action_prob(self, states, actions):
         """
 
         """
-        #actions = actions.type(torch.long).squeeze(1)
+        actions = actions.type(torch.long)
         #actions = actions.unsqueeze(0)
         # actions = actions.type(torch.long).view(-1)
-        actions = actions.type(torch.long)
+        # print("actions ", actions.shape)
+        
+        # actions = actions.type(torch.long).squeeze(1)
         
         # check if action prob is zero
         """
@@ -152,45 +157,45 @@ class Agent():
             return action_prob
         """
         output = self.predicter(states.unsqueeze(0))
+        output = F.softmax(output, dim=2)
         output = output.squeeze(0)
-        # print("output shape ", output.shape)
-        # print("action shape ", actions.shape)
+        #print("output shape ", output.shape)
+        #print("action shape ", actions.shape)
         action_prob = output.gather(1, actions)
         #print("action pob old {} ".format(action_prob, actions))
-        action_prob = action_prob.detach() + torch.finfo(torch.float32).eps
+        # action_prob = action_prob.detach() + torch.finfo(torch.float32).eps
         #print("action_prob ", action_prob.shape)
         #print("action pob {} ".format(action_prob, actions))
         action_prob = torch.log(action_prob)
+        if torch.isnan(action_prob).any():
+            print(output)
+            sys.exit()
         # print("action log {:2f} of action {} ".format(action_prob.item(), actions.item()))
         return action_prob
 
-    def compute_q_function(self, states, next_states,  actions):
+    def compute_q_function(self, states, next_states,  actions, dones):
         """
         
         """
         #print("")
         #print("update q function")
-        actions = actions.type(torch.float)
-        q_est = self.Q_local(states, actions)
-        r_pre = self.R_target(states, actions)
-        q_hat =  torch.empty((self.batch_size, 1), dtype=torch.float32).to(self.device)
-        for idx, s in enumerate(next_states):
-            q = []
-            for action in self.all_actions:
-                res =  self.Q_target(s.unsqueeze(0), action.unsqueeze(0))
-                q.append(res)
-                # print("action {} : q {} ".format(action.item(), res.item()))
-            q_max = max(q)
-            q_hat[idx]=  q_max
-        q_hat *= self.gamma
-        q_target = r_pre + q_hat
-        #print("q pre ", q_est)
-        #print("q target ", q_target)
-        q_loss = F.mse_loss(q_est, q_target)
+        actions = actions.type(torch.int64)
+        q_est = self.Q_local(states).gather(1, actions).squeeze(1)
+        #r_pre = self.R_target(states).gather(1, actions).squeeze(1)
+        r_pre = self.R_target(states).gather(1, actions).squeeze(1)
+        Q_target_next = self.Q_target(next_states).detach().max(1)[0]
+        #print("re ", r_pre.shape)
+        #print("tar", Q_target_next.shape)
+        # target_Q = r_pre + (self.gamma * Q_target_next * (1 - dones))
+        target_Q = r_pre + (self.gamma * Q_target_next)
+        
+        #print("q pre ", q_est.shape)
+        #print("q target ", target_Q.shape)
+        q_loss = F.mse_loss(q_est, target_Q)
         # Minimize the loss
         self.optimizer_q.zero_grad()
         q_loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.Q_local.parameters(), 1)
+        #torch.nn.utils.clip_grad_norm_(self.Q_local.parameters(), 1)
         self.optimizer_q.step()
         self.writer.add_scalar('Q_loss', q_loss, self.steps)
         #print("q update")
@@ -203,21 +208,21 @@ class Agent():
         
         """
         # compute difference between Q_shift and y_sh
-        actions = actions.type(torch.float)
-        q_sh_value = self.q_shift_local(states, actions)
-        y_sh =  torch.empty((self.batch_size, 1), dtype=torch.float32).to(self.device)
-        for idx, s in enumerate(next_states):
-            q = []
-            for action in self.all_actions:
-                q.append(self.Q_target(s.unsqueeze(0), action.unsqueeze(0)))
-            q_max = max(q)
-            y_sh[idx]=  q_max
-        y_sh *= self.gamma
-        q_shift_loss = F.mse_loss(y_sh, q_sh_value)
+        
+        actions = actions.type(torch.int64)
+        q_sh_value = self.q_shift_local(states).gather(1, actions).squeeze(1)
+        #print("shape ", q_sh_value.shape)
+        target_Q = self.Q_target(next_states).detach().max(1)[0]
+        #print("target, ", target_Q.shape)
+        
+        
+        
+        target_Q *= self.gamma 
+        q_shift_loss = F.mse_loss(q_sh_value, target_Q)
         # Minimize the loss
         self.optimizer_q_shift.zero_grad()
         q_shift_loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.q_shift_local.parameters(), 1)
+        # torch.nn.utils.clip_grad_norm_(self.q_shift_local.parameters(), 1)
         self.optimizer_q_shift.step()
         # print("q shift update")
 
@@ -227,11 +232,11 @@ class Agent():
         """
         
         """
-        actions = actions.type(torch.float)
-        y = self.R_local(states, actions)
-        y_shift = self.q_shift_target(states, actions)
+        actions = actions.type(torch.int64)
+        y = self.R_local(states).gather(1, actions).squeeze(1).unsqueeze(1)
+        y_shift = self.q_shift_target(states).gather(1, actions)
         y_r_part1 = self.get_action_prob(states, actions) - y_shift
-        #print("n_a ", y_r_part1)
+        #print("n_a ", y_r_part1.shape)
         # sum all other actions
         y_r_part2 =  torch.empty((self.batch_size, 1), dtype=torch.float32).to(self.device)
         idx = 0
@@ -241,9 +246,11 @@ class Agent():
                 if torch.eq(a, b):
                     continue
                 # print("diff ac ", b)
-                r_hat = self.R_target(s.unsqueeze(0), b.unsqueeze(0))
-                #print("r hat ", r_hat)
-                n_b = self.get_action_prob(s.unsqueeze(0), b.unsqueeze(0), True) - self.q_shift_target(s.unsqueeze(0), b.unsqueeze(0))
+                b = b.type(torch.int64).unsqueeze(1)
+                r_hat = self.R_target(s.unsqueeze(0)).gather(1, b)
+                
+                y_s = self.q_shift_target(s.unsqueeze(0)).gather(1, b)
+                n_b = self.get_action_prob(s.unsqueeze(0), b) - y_s
                 #print("action", b)
                 #print("n_b hat ", n_b )
                 y_h += (r_hat - n_b)
@@ -256,16 +263,15 @@ class Agent():
         #print("action ", actions)
         #print("na part 1 ", y_r_part1)
         #print("sum part 2 ", y_r_part2)
-        #sys.exit()
         #print("Update reward for action ", actions.item())
         #print("predict ", y)
         y_r = y_r_part1 + y_r_part2
-        #print("target ", y_r)
+        #print("target ", y_r.shape)
         r_loss = F.mse_loss(y, y_r)
         # Minimize the loss
         self.optimizer_r.zero_grad()
         r_loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.R_local.parameters(), 1)
+        # torch.nn.utils.clip_grad_norm_(self.R_local.parameters(), 1)
         self.optimizer_r.step()
         self.writer.add_scalar('Reward_loss', r_loss, self.steps)
 
@@ -281,11 +287,8 @@ class Agent():
 
     def act(self, state):
         state = torch.Tensor(state).to(self.device)
-        qs = []
-        for a in self.all_actions:
-            qs.append(self.Q_local(state.unsqueeze(0), a.unsqueeze(0)))
-
-        return np.argmax(qs)
+        action =torch.argmax(self.Q_local(state.unsqueeze(0)))
+        return action.item()
 
     def eval_policy(self, env, episode=2):
         scores = 0
@@ -295,9 +298,6 @@ class Agent():
             while True:
                 action = self.act(state)
                 state = torch.Tensor(state).to(self.device)
-                for a in self.all_actions:
-                    print("q function ", self.Q_local(state.unsqueeze(0), a.unsqueeze(0)))
-                print("action", action)
                 next_state, reward, done, _ = env.step(action)
                 state = next_state
                 score += reward
@@ -315,18 +315,22 @@ class Agent():
     def test_q_value(self, memory):
         same_action = 0
         for i in range(100):
-            states, next_states, actions = memory.expert_policy(1)
-            q_values = []
-            for a in self.all_actions:
-                q_values.append(self.Q_local(states, a.unsqueeze(0)))
-            best_action = np.argmax(q_values)
+            states, next_states, actions, dones = memory.expert_policy(1)
+            q_values = self.Q_local(states)
+            best_action = torch.argmax(q_values).item()
             self.debug(best_action)
             if  actions[0][0].item() == best_action:
                 same_action += 1
+            else:
+                print("Action expert ", actions[0][0].item())
+                print("Q values ", q_values)
+                print("Action prob ",  self.predicter(states.unsqueeze(0)))
         print("    ")
         al = self.debug(None)
         print("inverse action a0: {:.2f} a1: {:.2f} a2: {:.2f} a3: {:.2f}".format(al[0], al[1], al[2], al[3]))
-        print("Q values a0: {:.2f} a1: {:.2f} a2: {:.2f} a3: {:.2f}".format(q_values[0][0][0].data, q_values[1][0][0].data, q_values[2][0][0].data, q_values[3][0][0].data))
+        #print("Q values a0: {:.2f} a1: {:.2f} a2: {:.2f} a3: {:.2f}".format(q_values[0][0][0].item(), q_values[1][0][0].item(), q_values[2][0][0].item(), q_values[3][0][0].item()))
+        print(q_values)
+        #print("Q values a0: {:.2f} a1: {:.2f} a2: {:.2f} a3: {:.2f}".format(q_values[0][0][0], q_values[1][0][0], q_values[2][0][0], q_values[3][0][0]))
         print("same action {} of 100".format(same_action))
         self.average_same_action.append(same_action)
         av_action = np.mean(self.average_same_action)

models.py
@@ -2,46 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torch.distributions import Categorical
-
-
-
-
-class PolicyNetwork(nn.Module):
-    """Actor (Policy) Model."""
-
-    def __init__(self, state_size, action_dim, seed, fc1_units=64, fc2_units=64):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
-        """
-        super(PolicyNetwork, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_dim)
-
-    def forward(self, state):
-        """Build a network that maps state -> action values."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        logits = torch.softmax(x, dim=1)
-        return logits
-
-
-    def sample_action(self, state):
-        output = self.forward(state)
-        dis = Categorical(output)
-        action = dis.sample()
-        log_probs  = dis.log_prob(action)
-        ent = dis.entropy()
-        return output, action, log_probs, ent
 
 
 class RNetwork(nn.Module):
@@ -59,14 +19,13 @@ class RNetwork(nn.Module):
         """
         super(RNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size + action_dim, fc1_units)
+        self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, 1)
+        self.fc3 = nn.Linear(fc2_units, action_dim)
 
-    def forward(self, state, action):
+    def forward(self, state):
         """Build a network that maps state -> action values."""
-        sa = torch.cat((state, action), dim=1)
-        x = F.relu(self.fc1(sa))
+        x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
 
@@ -88,21 +47,20 @@ class QNetwork(nn.Module):
         """
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size + action_size, fc1_units)
+        self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
 
-    def forward(self, state, action):
+    def forward(self, state):
         """Build a network that maps state -> action values."""
-        sa = torch.cat((state, action), dim=1)
-        x = F.relu(self.fc1(sa))
+        x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
 
 class Classifier(nn.Module):
     """ Classifier Model."""
 
-    def __init__(self, state_size, action_dim, seed, fc1_units=64, fc2_units=64):
+    def __init__(self, state_size, action_dim, seed, fc1_units=128, fc2_units=128):
         """Initialize parameters and build model.
         Params
         ======
@@ -123,4 +81,5 @@ class Classifier(nn.Module):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         output = self.fc3(x)
+        return output
         return F.softmax(output, dim=2)

replay_buffer2.py
@@ -62,8 +62,9 @@ class ReplayBuffer(object):
         obses = torch.as_tensor(obses, device=self.device)
         next_obses = torch.as_tensor(next_obses, device=self.device)
         actions = torch.as_tensor(self.actions[idxs], device=self.device)
+        dones = torch.as_tensor(self.not_dones_no_max[idxs], device=self.device)
 
-        return obses, next_obses, actions
+        return obses, next_obses, actions, dones
 
 
 